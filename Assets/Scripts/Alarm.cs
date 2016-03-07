9ec5753fdd08f8ba77e404610bca2fa1493c7c69
Justin Wilkinson <wilkinsense@gmail.com>
2016-03-07 10:41:58 -0500

    Midterm commit.

---
+++ Assets/Scripts/Alarm.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class Alarm : MonoBehaviour {
+  public GameObject ball;
+
+  // Use this for initialization
+  void Start() {
+
+  }
+}
---
+++ Assets/Scripts/RollingSound.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RollingSound : MonoBehaviour {
+  Rigidbody _rigidbody;
+  int _collisionCount = 0;
+
+	// Use this for initialization
+	void Start () {
+    _rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if(_collisionCount > 0) {
+      /*if(audioSource.isPlaying == false) {
+        audioSource.Play();
+      }*/
+
+      float volume = Mathf.Clamp(_rigidbody.velocity.magnitude, 0.0f, 1.0f);
+      // set volume.
+
+      //audioSource.volume = volume;
+    }
+    else {
+      /*if(audioSource.isPlaying) {
+        audioSource.Stop();
+      }*/
+    }
+  }
+
+  public void OnCollisionEnter() {
+    _collisionCount++;
+  }
+
+  public void OnCollisionExit() {
+    _collisionCount--;
+  }
+
+}
