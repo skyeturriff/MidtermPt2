0c4f58087b27d8b71236fb479afc3e554aa7e71e
skyeturriff <turriff.skye@gmail.com>
2016-03-07 11:20:05 -0500

    Add main menu scene

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour
+{
+    // On button click, opens the specified scene
+    public void ChangeSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
